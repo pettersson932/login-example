9967c3e6872fddc969e8c3b812a8970150f3761b
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2024-10-07 18:25:38 +0200

    Medd till github

--- functions/user/Signup/index.js
+++ functions/user/Signup/index.js
@@ -20,7 +20,7 @@ exports.handler = async (event) => {
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: "all good.",
+        message: username,
       }),
     };
   } catch (error) {
